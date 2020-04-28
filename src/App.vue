48e2a80aebd228dca642e3ee1963c9c28c2327eb
adrianoadvrio <adrianoadvrio@yahoo.com.br>
2020-04-28 11:10:10 -0300

    done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,152 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">Produtos Front</a>
+      </div>
+    </nav>
+
+    <div class="container">
+
+        <p v-if="errors != ''">
+            <b>Por favor, corrija o(s) seguinte(s) erro(s):</b>
+            <ul>
+              <li v-for="(error, index) of errors" :key="index">{{ error }}</li>
+            </ul>
+          </p>
+    
+    <!--{{produto.nome}}-->
+       
+      <form @submit.prevent="salvar">
+
+          <label>Nome</label>
+          <input type="text" placeholder="Nome" v-model="produto.nome">
+          <label>Quantidade</label>
+          <input type="number" placeholder="QTD" v-model="produto.quantidade">
+          <label>Valor</label>
+          <input type="text" placeholder="Valor" v-model="produto.valor">
+
+          <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+
+      </form>
+
+      <table>
+
+        <thead>
+
+          <tr>
+            <th>NOME</th>
+            <th>QTD</th>
+            <th>VALOR</th>
+            <th>OPÇÕES</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+          <tr v-for="produto of produtos" :key="produto.id">
+
+            <td>{{produto.nome}}</td>
+            <td>{{produto.quantidade}}</td>
+            <td>{{produto.valor}}</td>
+            <td>
+              <button @click="editar(produto)" class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
+              <button @click="remover(produto)" class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+            </td>
+
+          </tr>
+
+        </tbody>
+      
+      </table>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Produto from './services/produtos'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  data(){
+return{
+  produto: {
+    id:'',
+    nome: '',
+    quantidade: '',
+    valor: ''
+  },
+  produtos: [],
+  errors: []
 }
+  },
+
+mounted() {
+this.listar()
+},
+
+methods:{
+
+  listar(){
+Produto.listar().then(resposta => {
+this.produtos = resposta.data
+})
+  },
+
+salvar(){
+
+  if(!this.produtos.id){
+
+Produto.salvar(this.produto).then(resposta => {
+  this.produto = {}
+  this.produto = resposta
+alert('Guardado con suceso')
+this.listar()
+this.errors = []
+
+}).catch(e => {
+    this.errors = e.response.data.errors
+})
+  }else{
+    Produto.atualizar(this.produto).then(resposta => {
+  this.produto = {}
+  this.produto = resposta
+alert('Actualizado con suceso')
+this.listar()
+this.errors = []
+}).catch(e => {
+    this.errors = e.response.data.errors
+  })
+  }
+},
+
+editar(produto){
+this.produto = produto
+},
+
+remover(produto){
+  if(confirm('¿desea eliminar?')){
+  Produto.apagar(produto).then(resposta =>{
+    this.produto = resposta
+this.listar();
+this.errors = []
+  }).catch(e => {
+    this.errors = e.response.data.errors
+  })
+}
+
+}
+
+}
+}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
